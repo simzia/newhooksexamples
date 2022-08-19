ccf8c471fb26f35f421c5ea456ca7c74163a57d0
aayushi <aayushi.simzia@cygbit.com>
2022-08-19 17:19:22 +0530

    first commit

--- src/components/context/ThemeContext.tsx
+++ src/components/context/ThemeContext.tsx
@@ -7,5 +7,5 @@ type ThemeContextProviderProps = {
 export const ThemeContext = createContext(theme)
 
 export const ThemeContextProvider = ({children}: ThemeContextProviderProps) => {
-    return <ThemeContext.Provider value={theme  }>{children}</ThemeContext.Provider>
+    return <ThemeContext.Provider value={theme}>{children}</ThemeContext.Provider>
 }
